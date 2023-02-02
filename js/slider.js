4bbb22bc27397c7117bf020f0c4a977898156a7f
Lee Jun Moo <113665599+leejunmoo@users.noreply.github.com>
2023-02-02 15:35:48 +0900

    슬라이더 오류 수정
    새로고침해야만 반응형에 적용되던거 실시간을 변경

--- js/slider.js
+++ js/slider.js
@@ -17,40 +17,53 @@ var swiper = new Swiper(".mySwiperbanner", {
     },
   });
 
-
-  /* pc버전 책 슬라이더 */
-  if (window.matchMedia("(min-width: 767px)").matches) {
-    var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        slidesPerGroup: 3,
-        loop: true,
-        loopFillGroupWithBlank: true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      });
-  } else { // 모바일 버전 책슬라이더
-    var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 1,
-        spaceBetween: 0,
-        slidesPerGroup: 1,
-        loop: true,
-        loopFillGroupWithBlank: true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      });
-      console.log('mobile')
+  let ww = $(window).width();
+  function webSwiper() {
+      swiper = new Swiper(".mySwiper", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      slidesPerGroup: 3,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
   }
-  
+  function mobileSwiper() {
+      swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      slidesPerGroup: 1,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  }
+  /* pc버전 책 슬라이더 */
+  if (ww > 767) {
+    webSwiper();
+  } else { // 모바일 버전 책슬라이더
+    mobileSwiper();
+  }
+
+  $(window).on('resize', function() {
+    ww = $(window).width()
+    if ( ww > 767 ) {
+      webSwiper();
+    } else {
+      mobileSwiper();
+    }
+  })
